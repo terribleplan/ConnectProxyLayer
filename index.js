7e71d642005e290a19ae09fc5d0e4632d3592a7b
Kegan Myers <kegan@keganmyers.com>
2014-04-21 17:03:24 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+//Load up what we always require
+var qs = require('qs'), url = require('url');
+
+//And provide a way to pass in options and get back a piece of connect middleware
+module.exports = function middleware(endpoint, input, output) {
+
+    //Figure out where and how we will be sending our request
+    var parsedEndpoint = (typeof endpoint === "string" ? url.parse(endpoint) : endpoint);
+    var parsedPath = ((parsedEndpoint.pathname.slice(-1) === "/") ? parsedEndpoint.pathname.slice(0, -1) : parsedEndpoint.pathname.slice(-1));
+    var reqLib = (parsedEndpoint.protocol === "https:") ? require('https') : require('http');
+
+    //Make sure we maintain context and don't rely on being called with the proper one
+    //Also don't use bind because it is so slow, :(
+    function createProxy(context) {
+        return function (localRequest, requestBody, localResponse, nxt) {
+            proxy(localRequest, requestBody, localResponse, nxt, context);
+        }
+    }
+
+    function sendOutput(localResponse, remoteResponse, responseBody) {
+        localResponse.writeHead(remoteResponse.statusCode, remoteResponse.headers);
+        localResponse.end(responseBody);
+    }
+
+    //The meat and potatoes, here is where the request will be sent to the remote server, and the response read
+    function proxy(localRequest, requestBody, localResponse, nxt, context) {
+
+        if (typeof localRequest.query === "object") {
+            var parsed = url.parse(localRequest.url);
+            localRequest.url = parsed.pathname + "?" + qs.stringify(localRequest.query);
+        }
+
+        var requestLocation = parsedPath + localRequest.url;
+        localRequest.headers["host"] = parsedEndpoint.hostname + ":" + parsedEndpoint.port;
+
+        var req = {
+            headers: localRequest.headers,
+            host: parsedEndpoint.hostname,
+            port: parsedEndpoint.port,
+            path: requestLocation,
+            method: localRequest.method
+        };
+
+        var remoteRequest = reqLib.request(req, function (remoteResponse) {
+
+            //Read in all of the data
+            var data = "";
+            remoteResponse.on('data', function (chunk) {
+                data += chunk;
+            });
+
+            remoteResponse.on('error', function (err) {
+                nxt(err);
+            });
+
+            remoteResponse.on('end', function () {
+                delete remoteResponse.headers['content-encoding'];
+                output.call(context, remoteResponse, data, localResponse, nxt, sendOutput);
+            });
+        });
+
+        remoteRequest.on('error', function (err) {
+            nxt(err);
+        });
+
+        remoteRequest.end(requestBody);
+    }
+
+    //Ensure that our interceptors are at least a no-op
+    if (typeof input !== "function") {
+        input = function input(localRequest, requestBody, localResponse, nxt, proxy) {
+            proxy(localRequest, requestBody, localResponse, nxt);
+        }
+    }
+
+    if (typeof output !== "function") {
+        output = function output(remoteResponse, responseBody, localResponse, nxt, send) {
+            send(localResponse, remoteResponse, responseBody);
+        }
+    }
+
+    return function (localRequest, localResponse, next) {
+
+        //Create a local scope
+        var interceptContext = {};
+
+        var localBody = "";
+        localRequest.on('data', function (chunk) {
+            localBody += chunk;
+        });
+
+        localRequest.on('error', function (err) {
+            next(err);
+        });
+
+        localRequest.on('end', function () {
+            //And kick off the process with it
+            input.call(interceptContext, localRequest, localBody, localResponse, next, createProxy(interceptContext));
+        });
+    }
+};
